946a4e467d27505b378b1fa5f275826abd4caf3d
Kendall <KENDALL.MENDEZCALDERON@ucr.ac.cr>
2026-09-20 22:05:43 -0600

    feat: ajuste de estructura del conductor

--- src/main/java/cr/ac/ucr/paraiso/ie/c4h142/expresofast/domain/Conductor.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/c4h142/expresofast/domain/Conductor.java
@@ -1,10 +1,17 @@
 package cr.ac.ucr.paraiso.ie.c4h142.expresofast.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Conductor")
@@ -31,17 +38,63 @@ public class Conductor {
     @OneToMany(mappedBy = "conductor")
     private List<Envio> envios = new ArrayList<>();
 
+    @Column(nullable = false)
+    private Boolean activo;
+
     // Getters y setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getApellidos() { return apellidos; }
-    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
-    public String getLicencia() { return licencia; }
-    public void setLicencia(String licencia) { this.licencia = licencia; }
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
-    public List<Envio> getEnvios() { return envios; }
-    public void setEnvios(List<Envio> envios) { this.envios = envios; }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Envio> getEnvios() {
+        return envios;
+    }
+
+    public void setEnvios(List<Envio> envios) {
+        this.envios = envios;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
